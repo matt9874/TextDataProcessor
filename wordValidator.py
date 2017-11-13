1bade2c0618ae5894679676f1985ea53e0fb3466
matt9874 <mattcave623@gmail.com>
2017-11-13 08:17:56 +0000

    Use variable to control whether stopwords are removed

--- wordValidator.py
+++ wordValidator.py
@@ -6,5 +6,6 @@ from nltk.corpus import stopwords
 class WordValidator:
     def __init__(self):
         self.stopWords=set(stopwords.words('english'))
+        self.removeStopWordsFromResults=True
     def Validate(self, word):
-        return not word in self.stopWords
+        return not (word in self.stopWords and self.removeStopWordsFromResults)
