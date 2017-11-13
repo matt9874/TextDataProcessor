845e81b3de5913b8576dca975bf0c1cd21e3d0aa
matt9874 <mattcave623@gmail.com>
2017-11-13 07:59:51 +0000

    Use nltk english stopwords to validate words

--- wordValidator.py
+++ wordValidator.py
@@ -1,5 +1,10 @@
+
+#import nltk
+#nltk.download("stopwords")
+from nltk.corpus import stopwords
+
 class WordValidator:
     def __init__(self):
-        self.disallowedWords=set('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
+        self.stopWords=set(stopwords.words('english'))
     def Validate(self, word):
-        return not word in self.disallowedWords
+        return not word in self.stopWords
