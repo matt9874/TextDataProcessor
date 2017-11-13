b65cd3462d7bc43d53a8f4328a5c588a783aa35f
matt9874 <mattcave623@gmail.com>
2017-11-13 07:57:16 +0000

    Add variable to set max number of results displayed in output

--- textFilesOutputter.py
+++ textFilesOutputter.py
@@ -1,11 +1,15 @@
 import tabulate as tab
 
 class TextFilesOutputter:
+    def __init__(self):
+        self.numResults=5
     def Display(self,textDataRecords):
         textDataRecords.sort(key=lambda x: x.count, reverse=True)
         formattedData=[]
         numWords=len(textDataRecords)
-        for i in range(0,numWords):
+        if (type(self.numResults) is int and self.numResults>=0 and self.numResults<numWords):
+            outputSize=self.numResults
+        for i in range(0,outputSize):
             wordData=textDataRecords[i]
             formattedData.append([wordData.word, wordData.count, ', '.join(wordData.containingDocuments), '\n\n'.join(wordData.containingSentences)])
         print tab.tabulate(formattedData, headers=['Word', 'Count', 'Documents', 'Sentences containing the word'], tablefmt='grid')
